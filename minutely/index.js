b1e26fae24c5c5aa46ee223006dd4e15a0d7d81a
Andrew Kreps <andrew.kreps@gmail.com>
2016-11-02 23:51:29 -0700

    Tweaking minutely to work.

--- minutely/index.js
+++ minutely/index.js
@@ -1,5 +1,10 @@
-var Darksky = require('node-darksky');
+var Darksky = require('darksky-node');
 
 exports.handler = function(event, context) {
     console.log(event);
+    context.succeed({
+        statusCode: 200,
+        headers: {},
+        body: ""
+    })
 };
