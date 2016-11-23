352d84077efa902ac9fdfbf60a75d11c00d65323
Andrew Kreps <andrew.kreps@gmail.com>
2016-11-23 00:22:04 -0800

    Minutely probability

---
+++ minutely_probability/index.js
@@ -0,0 +1,18 @@
+var superagent = require('superagent');
+
+exports.handler = function(event, context) {
+    console.log(event.queryStringParameters);
+
+    // Call the geocoder with the loc
+    superagent.get(process.env.MINUTELY_URI + event.queryStringParameters.loc)
+        .end(function (err, forecast) {
+            console.log("Forecast result: " + JSON.stringify(forecast.body));
+            // Get the forecast
+            Object.keys(forecast.body.data).map((e) => console.log(`key=${e}  value=${forecast.body.data[e]}`));
+            context.succeed({
+                statusCode: 200,
+                headers: {},
+                body: JSON.stringify(forecast.body)
+            });
+        });
+};
