767a9f0ad5226992d5b406799eebf7c8020a8db2
Andrew Kreps <andrew.kreps@gmail.com>
2016-11-24 22:58:08 -0800

    Hourly intensity

---
+++ hourly_temp/index.js
@@ -0,0 +1,18 @@
+var superagent = require('superagent');
+
+exports.handler = function(event, context) {
+    console.log(event.queryStringParameters);
+
+    // Call the geocoder with the loc
+    superagent.get(process.env.FORECAST_URI + event.queryStringParameters.loc)
+        .end(function (err, forecast) {
+            console.log("Forecast result: " + JSON.stringify(forecast.body.hourly));
+            var hourly_temps = forecast.body.hourly.data.map(function(hour) {return hour.temperature;});
+
+            context.succeed({
+                statusCode: 200,
+                headers: {},
+                body: JSON.stringify(hourly_temps)
+            });
+        });
+};
