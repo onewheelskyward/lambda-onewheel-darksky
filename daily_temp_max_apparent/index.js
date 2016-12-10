7ec9332332dc8b4819a64edaaad0e03f5c723819
Andrew Kreps <andrew.kreps@gmail.com>
2016-12-10 14:43:34 -0800

    Daily totals and apparents.

---
+++ daily_temp_max_apparent/index.js
@@ -0,0 +1,19 @@
+var superagent = require('superagent');
+
+exports.handler = function(event, context) {
+    console.log(event.queryStringParameters);
+
+    // Call the geocoder with the loc
+    console.log("Calling " + process.env.FORECAST_URI + event.queryStringParameters.loc);
+    superagent.get(process.env.FORECAST_URI + event.queryStringParameters.loc)
+        .end(function (err, forecast) {
+            console.log("Forecast result: " + JSON.stringify(forecast.body.hourly));
+            var daily = forecast.body.daily.data.map(function(day) {return day.apparentTemperatureMax;});
+
+            context.succeed({
+                statusCode: 200,
+                headers: {},
+                body: JSON.stringify(daily)
+            });
+        });
+};
